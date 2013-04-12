2c68dadb92b7b149041b45cfab128b782c5c5765
Vitor Baptista <vitor@vitorbaptista.com>
2013-04-12 20:57:35 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dadoseducacionais::Application.config.secret_token = '37611b7d514569f81f743f4514da46e4a42ab8dbc981c30ad4051d4459361170de8cf6c81c293beb49c2a466114b44147cc1580fbd74345260fc090eaeef8ea8'
