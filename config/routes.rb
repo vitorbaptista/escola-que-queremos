e3e75afb42742a55f713f9131e79f68dd63d259a
Vitor Baptista <vitor@vitorbaptista.com>
2013-04-14 05:31:59 -0300

    Add menu

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Dadoseducacionais::Application.routes.draw do
   match '/school/info/:id' => 'school#info'
   match '/school/search' => 'school#search'
   match '/school/your_indicator/:id' => 'school#your_indicator'
+  match '/sobre' => 'home#about'
 
   root :to => 'school#index'
 end
