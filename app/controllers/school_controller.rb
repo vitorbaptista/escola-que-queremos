cfbcfca6339892272be139b9d6a4807ca4564618
Adriano Bonat <adrianob@gmail.com>
2013-04-13 23:03:38 -0300

    updated counts since we filtered some schools.

--- app/controllers/school_controller.rb
+++ app/controllers/school_controller.rb
@@ -1,15 +1,15 @@
 # encoding: utf-8
 class SchoolController < ApplicationController
   def index
-    @total_schools = 263833
+    @total_schools = 194932
     @categories = [
       {title: 'Estrutura Física',
-       indicators: [{name: 'Estrutura Básica', description: 'Água, luz, banheiro e saneamento básico', count: 160296},
-                    {name: 'Biblioteca', description: '', count: 263833},
-                    {name: 'Sala de Informática', description: 'Com acesso à internet para os alunos', count: 66918},
-                    {name: 'Quadra de Esportes', description: '', count: 52045}]},
+       indicators: [{name: 'Estrutura Básica', description: 'Água, luz, banheiro e saneamento básico', count: 159853},
+                    {name: 'Biblioteca', description: '', count: 65553},
+                    {name: 'Sala de Informática', description: 'Com acesso à internet para os alunos', count: 66743},
+                    {name: 'Quadra de Esportes', description: '', count: 51924}]},
       {title: 'Insumos e condições de funcionamento da escola',
-       indicators: [{name: 'Merenda', description: '', count: 263833},
+       indicators: [{name: 'Merenda', description: '', count: 168803},
                     {name: 'Merenda de Qualidade', description: '', count: 2457},
                     {name: 'Projeto Pedagógico', description: '', count: 54129},
                     {name: 'Livro didático', description: 'Para todos alunos', count: 18758}]},
@@ -19,7 +19,7 @@ class SchoolController < ApplicationController
                     {name: 'Projeto Pedagógico Democrático', description: '', count: 22862},
                     {name: 'Apoio da Comunidade', description: '', count: 52113}]},
       {title: 'Formação e Condições de Trabalho dos Profissionais da Escola',
-       indicators: [{name: 'Formação Inicial', description: '', count: 55244},
+       indicators: [{name: 'Formação Inicial', description: '', count: 4668},
                     {name: 'Equipe Pedagógica Completa', description: '', count: 46616},
                     {name: 'Turmas Com Até 25 alunos', description: ''},
                     {name: 'Salário Acima do Piso', description: ''}]},
