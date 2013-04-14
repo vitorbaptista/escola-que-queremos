a93959056edbe908ea44b91f914636bb57b509ac
Adriano Bonat <adrianob@gmail.com>
2013-04-14 06:04:52 -0300

    added help pages.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+class HomeController < ApplicationController
+  def about
+  end
+
+  def how_to_use
+  end
+
+  def next_steps
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Dadoseducacionais::Application.routes.draw do
   match '/school/info/:id' => 'school#info'
   match '/school/search' => 'school#search'
   match '/school/your_indicator/:id' => 'school#your_indicator'
+
   match '/sobre' => 'home#about'
+  match '/como-usar' => 'home#how_to_use'
+  match '/proximos-passos' => 'home#next_steps'
 
   root :to => 'school#index'
 end
